50ecf7b4b22604f3aec6266590efd1439235c590
sulishun <1493034020@qq.com>
2024-05-24 12:56:55 +0800

    修改了参数名称大小写

--- DrawNDAppCenter.cpp
+++ DrawNDAppCenter.cpp
@@ -2,31 +2,30 @@
 #include <math.h>
 #include<stdio.h>
 
-extern double rotationAngle;// 用于传入用
-double realRotationAngle = 0;
-double pointRtationAngle = 0;// 小剑的对应角度
+extern double rotationangle;// 用于传入用
+double realrotationangle = 0;
+double pointrtationangle = 0;// 小剑的对应角度
 extern int HDGMAG;// 这个参数你应该没设
 
 void draw_smallCircle(double x, double y, double r) {
 	setfillcolor(WHITE);
-	ege_ellipse(x + r * 0.45 * cos(-(pointRtationAngle + realRotationAngle) * PI / 180) - r * 0.025, 
-		y - r * 0.025 - r * 0.3 * sin(-(pointRtationAngle + realRotationAngle) * PI / 180), r * 0.05, r * 0.05);
-	ege_ellipse(x - r * 0.45 * cos(-(pointRtationAngle + realRotationAngle) * PI / 180) - r * 0.025,
-		y - r * 0.025 + r * 0.3 * sin(-(pointRtationAngle + realRotationAngle) * PI / 180), r * 0.05, r * 0.05);
-	ege_ellipse(x + r * 0.25 * cos(-(pointRtationAngle + realRotationAngle) * PI / 180) - r * 0.025, 
-		y - r * 0.025 - r * 0.6 * sin(-(pointRtationAngle + realRotationAngle) * PI / 180), r * 0.05, r * 0.05);
-	ege_ellipse(x - r * 0.25 * cos(-(pointRtationAngle + realRotationAngle) * PI / 180) - r * 0.025, 
-		y - r * 0.025 + r * 0.6 * sin(-(pointRtationAngle + realRotationAngle) * PI / 180), r * 0.05, r * 0.05);
+	ege_ellipse(x + r * 0.45 * cos(-(pointrtationangle + realrotationangle) * PI / 180) - r * 0.025, 
+		y - r * 0.025 - r * 0.3 * sin(-(pointrtationangle + realrotationangle) * PI / 180), r * 0.05, r * 0.05);
+	ege_ellipse(x - r * 0.45 * cos(-(pointrtationangle + realrotationangle) * PI / 180) - r * 0.025,
+		y - r * 0.025 + r * 0.3 * sin(-(pointrtationangle + realrotationangle) * PI / 180), r * 0.05, r * 0.05);
+	ege_ellipse(x + r * 0.25 * cos(-(pointrtationangle + realrotationangle) * PI / 180) - r * 0.025, 
+		y - r * 0.025 - r * 0.6 * sin(-(pointrtationangle + realrotationangle) * PI / 180), r * 0.05, r * 0.05);
+	ege_ellipse(x - r * 0.25 * cos(-(pointrtationangle + realrotationangle) * PI / 180) - r * 0.025, 
+		y - r * 0.025 + r * 0.6 * sin(-(pointrtationangle + realrotationangle) * PI / 180), r * 0.05, r * 0.05);
 }
 
 void draw_ND_app_center(double center_x, double center_y, double side) {
-	setfillcolor(BLACK);
-	ege_fillrect(center_x-side/2,center_y - side/2,side,side);
+	
 	// 初始数据设置
 	double r = side / 115 * 37.5;
-	realRotationAngle = 360 - rotationAngle;
+	realrotationangle = 360 - rotationangle;
 	setcolor(EGEARGB(200, 177, 177, 178));
-
+	
 	// 长长刻度
 	setlinewidth(side / 250);
 	for (double i = 45; i < 360; i += 45)
@@ -39,7 +38,7 @@ void draw_ND_app_center(double center_x, double center_y, double side) {
 	setlinewidth(side / 250);// 这是对长刻度的粗细设定
 	for (double i = 0; i < 360; i += 10)
 	{	// 长刻度对应比例：(1- 5/37.5)
-		double scaleAngle = realRotationAngle + i;
+		double scaleAngle = realrotationangle + i;
 		double rad = scaleAngle * PI / 180.0;
 		ege_line(center_x + r * sin(rad), center_y - r * cos(rad), center_x + (1- 5/37.5)* r * sin(rad), center_y - (1 - 5 / 37.5) * r * cos(rad));
 	}
@@ -47,29 +46,29 @@ void draw_ND_app_center(double center_x, double center_y, double side) {
 	setlinewidth(side/ 280);// 这是对短刻度的粗细设定
 	for (double i = 5; i < 360; i += 10)
 	{	// 短刻度对应比例： (1- 2/37.5)
-		double scaleAngle = realRotationAngle + i;
+		double scaleAngle = realrotationangle + i;
 		double rad = scaleAngle * PI / 180.0;
 		ege_line(center_x + r * sin(rad), center_y - r * cos(rad), center_x + (1 - 2 / 37.5) * r * sin(rad), center_y - (1 - 2 / 37.5) * r * cos(rad));
 	}
 	// 上下两个长刻度
 	ege_line(center_x,center_y - r,
 		center_x, center_y - (1 - 19/37.5)*r
-	);
+		);
 	ege_line(center_x, center_y + r,
 		center_x, center_y + (1 - 19 / 37.5) * r
-	);
+		);
 	ege_line(center_x - r / 32,center_y - (1 - 12 / 37.5) * r,
 		center_x + r / 32, center_y - (1 - 12 / 37.5) * r );
 	ege_line(center_x - r / 32, center_y + (1 - 12 / 37.5) * r,
 		center_x + r / 32, center_y + (1 - 12 / 37.5) * r);
-
+	
 	// 旋转数字绘制
 	for (int i = 0; i < 360; i += 30) {
-		double textAngle = realRotationAngle + i;
+		double textAngle = realrotationangle + i;
 		double rad = textAngle * PI / 180.0;
 		double textPosX = center_x + 0.75 * r * sin(rad);//*0.85是调节数字与中心坐标位置的关系，依据自己的位置条件适当修改，如可能在你的表上*0.7是恰好在需要的位置上，则改为*0.7
 		double textPosY = center_y - 0.75 * r * cos(rad);
-
+		
 		LOGFONTW font;
 		setfont(30, 0, "黑体");
 		settextjustify(CENTER_TEXT, CENTER_TEXT);
@@ -84,7 +83,7 @@ void draw_ND_app_center(double center_x, double center_y, double side) {
 		sprintf_s(strBuffer, "%d", i / 10);
 		ege_drawtext(strBuffer, textPosX, textPosY);
 	}
-
+	
 	// 上面小三角形
 	ege_point points1 [4] = {
 		center_x - side / 85, center_y - r / 0.91,// 左上
@@ -93,7 +92,7 @@ void draw_ND_app_center(double center_x, double center_y, double side) {
 		center_x - side / 85,center_y - r / 0.91
 	};
 	ege_drawpoly(4,points1);
-
+	
 	// 小三角形上面的3条线
 	ege_point points2[4] = {
 		center_x - side / 19, center_y - r / 0.8,// 左上
@@ -102,7 +101,7 @@ void draw_ND_app_center(double center_x, double center_y, double side) {
 		center_x + side / 19, center_y - r / 0.8// 右上
 	};
 	ege_drawpoly(4, points2);
-
+	
 	// 上面数字显示
 	setbkmode(TRANSPARENT);
 	settextjustify(CENTER_TEXT, CENTER_TEXT);
@@ -110,7 +109,7 @@ void draw_ND_app_center(double center_x, double center_y, double side) {
 	char ch[64];
 	sprintf(ch, "%03d", HDGMAG);
 	ege_drawtext(ch, center_x, center_y - r / 0.85);
-
+	
 	setcolor(EGEARGB(250, 76, 153, 0));// 这是对两边字体的颜色设置绿色
 	setfont(side / 25, 0, "黑体");// 这是对字体的大小设置
 	ege_drawtext("HDG", center_x - r / 3.5,center_y - r / 0.85);
@@ -131,11 +130,11 @@ void draw_ND_app_center(double center_x, double center_y, double side) {
 		center_x - r / 8, center_y + r / 7);
 	ege_line(center_x + r / 16, center_y + r / 7,
 		center_x + r / 8, center_y + r / 7);
-
+	
 	// 四个小圆绘制
 	draw_smallCircle(center_x,center_y,r);
-
-
+	
+	
 	setlinewidth(side / 250);
 	setcolor(EGEARGB(200, 177, 177, 178));
 	ege_point points3[14] = {
@@ -155,7 +154,7 @@ void draw_ND_app_center(double center_x, double center_y, double side) {
 		center_x - r / 30,center_y - r / 2.9//1
 	};
 	ege_drawpoly(14, points3);
-
+	
 	ege_point points4[6] = {
 		center_x - r / 30,center_y + r / 2.9,// 1
 		center_x + r / 30,center_y + r / 2.9,//2
