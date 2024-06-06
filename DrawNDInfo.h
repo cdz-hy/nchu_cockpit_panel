c1245d87feeac02fa6cacc819e57e6d04a969f27
cdz_hy <3116362036@qq.com>
2024-06-06 16:20:19 +0800

    ND航路显示完善

--- DrawNDInfo.h
+++ DrawNDInfo.h
@@ -8,3 +8,7 @@
 void draw_waypoint(WAYPOINT nowPos, double dir, double ratio,double x, double y, double side);
 
 void draw_route(WAYPOINT wp, double dir, double ratio,double x, double y, double side);
+
+void draw_waypoint_map(WAYPOINT nowPos, double dir, double ratio,double x, double y, double side);
+
+void draw_route_map(WAYPOINT wp, double dir, double ratio,double x, double y, double side);
