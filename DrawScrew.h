03e0a9b8f72d6d483dae4e46bc02f15d74cdb4d0
sulishun <1493034020@qq.com>
2024-07-03 17:53:47 +0800

    加一种螺丝

--- DrawScrew.h
+++ DrawScrew.h
@@ -5,3 +5,5 @@ void draw_screw(double screw_x, double screw_y, double screw_r, double angle);
 void draw_screw1(double screw_x, double screw_y, double screw_r, double angle);
 void draw_screw2(double screw_x, double screw_y, double screw_r, double angle);
 void draw_screw3(double screw_x, double screw_y, double screw_r, double angle);
+void draw_screw4(double screw_x, double screw_y, double screw_r, double angle);
+
